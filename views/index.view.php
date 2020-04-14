4e22e4ef99a6dd7b160881ceea6e9edcaa63f5fe
Mohamed ZARHOU <zarhou.cc@gmail.com>
2020-04-14 13:43:40 +0000

    add styles

--- views/index.view.php
+++ views/index.view.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Index</title>
+  <!-- styles -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
